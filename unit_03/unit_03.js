06077a6cbbc688427bcde3a4106ab63a2af28b8e
prodevo <rrvpievp@gmail.com>
2026-05-05 14:57:21 +0300

    Add unit 3

---
+++ unit_03/unit_03.js
@@ -0,0 +1,148 @@
+// Task 1
+/
+function f1(){
+   
+}
+
+document.querySelector('.b-1').onclick = f1;
+
+
+// Task 2
+
+
+function f2(){
+  
+}
+
+document.querySelector('.b-2').onclick = f2;
+
+
+// Task 3
+
+function f3(){
+   
+}
+
+document.querySelector('.b-3').onclick = f3;
+
+
+// Task 4. 
+
+
+function f4() {
+  
+}
+
+document.querySelector('.b-4').onclick = f4;
+
+// Task 5. 
+
+function f5(){
+   
+}
+
+
+document.querySelector('.b-5').onclick = f5;
+
+
+// Task 6.
+
+function f6(){
+   
+
+}
+
+document.querySelector('.b-6').onclick = f6;
+
+// Task 7.
+
+function f7(){
+   
+}
+
+document.querySelector('.b-7').onclick = f7;
+
+// Task 8.
+
+
+function f8() {
+  
+}
+
+document.querySelector('.b-8').onclick = f8;
+
+// Task 9
+
+function f9(){
+   
+
+}
+
+document.querySelector('.b-9').onclick = f9;
+
+// Task 10
+
+function f10(){
+   
+
+}
+
+document.querySelector('.b-10').onclick = f10;
+
+// Task 11
+
+function f11(){
+   
+
+}
+
+document.querySelector('.s-110').onchange = f11;
+
+
+ 
+ 
+ 
+
+// Task 12
+
+//let i120 = document.querySelector('.i-120');
+//let out_12 = document.querySelector('.out-12');
+
+//function f12(){
+//   let v = i120.value;
+//   out_12.innerHTML = (typeof v);
+
+//}
+
+//document.querySelector('.b-12').onclick = f12;
+
+// Task 13
+
+
+function f13() {
+   
+}
+
+document.querySelector('.b-13').onclick = f13;
+
+
+
+// Task 14
+
+
+function f14() {
+   
+}
+
+document.querySelector('.b-14').onclick = f14;
+
+// Task     15
+
+
+function f15() {
+  
+}
+
+document.querySelector('.b-15').onclick = f15;
+
+
